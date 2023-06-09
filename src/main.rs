025431e030686209b14b64c6b7f71c01a44c2ac2
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-06-09 06:43:54 +0100

    Refactored guess to struct

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,26 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess{
+        if value < 1 || value > 100 {
+            println!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -11,15 +31,11 @@ fn main() {
         println!("Please input the guess.");
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => Guess::new(num).value.try_into().unwrap(),
             Err(_) => continue,
         };
         println!("You guessed: {}", guess);
-
-        if guess < 1 || guess > 100 {
-            println!("The secret number is between 1 and 100");
-        }
 
         match guess.cmp(&secret_number){
             Ordering::Less => println!("Too Small!"),
